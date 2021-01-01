98270c1a7fb4e4403c70463db2b62c2331417f5c
Ameer Sameer <ammeer_atteya@yahoo.com>
2021-01-01 21:57:16 +0200

    initial commit

--- src/Controller/GithubController.php
+++ src/Controller/GithubController.php
@@ -23,9 +23,48 @@ class GithubController extends AbstractController
      */
     public function listRepos(Request $request, HttpClientInterface $client): Response
     {
-        $response = $client->request('GET', 'https://api.github.com/search/repositories?q=created:%3E2019-01-10&sort=stars&order=desc');
+        $queryParameters = $this->getQueryParameters($request);
+        if (!$this->validateQueryParameters($queryParameters)) {
+            return $this->json(['Not valid search parameters.']);
+        }
 
+        $response = $client->request('GET', $this->getParameter('GITHUB_REPOS_BASE_URL') . $this->getQueryParametersString($queryParameters));
         $content = $response->toArray();
+
         return $this->json($content);
+    }
+
+    protected function getQueryParameters(Request $request)
+    {
+        return [
+            'created_at' => $request->get('created_at') ?? '2019-01-10',
+            'sort' => $request->get('sort') ?? 'stars',
+            'order' => $request->get('order') ?? 'desc',
+            'per_page' => $request->get('per_page') ?? '10',
+            'language' => $request->get('language') ?? 'javascript',
+        ];
+    }
+
+    protected function validateQueryParameters($queryParameters)
+    {
+        $createdAt = $queryParameters['created_at'];
+        $createdAtArr = explode('-', $createdAt);
+        if (!count($createdAtArr) == 3) {
+            return false;
+        }
+        if (!checkdate($createdAtArr[1], $createdAtArr[2], $createdAtArr[0])) {
+            return false;
+        }
+        return true;
+    }
+
+    protected function getQueryParametersString($queryParameters)
+    {
+        $queryParametersString = "?q=created:%3E" . $queryParameters["created_at"];
+        unset($queryParameters['created_at']);
+        foreach ($queryParameters as $key => $value) {
+            $queryParametersString .= "&$key=$value";
+        }
+        return $queryParametersString;
     }
 }
